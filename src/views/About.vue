9e6777c5b185325b004f02b22853e4172c95a715
驴淡 <754886596@qq.com>
2018-11-30 13:54:15 +0800

    这个是新的

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,149 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div id="app">a={{ a }}, b={{ b }}</div>
+    <div>
+      <p>3333333333333333{{c}}</p>
+    </div>
+    <h1 v-show="ps">Hello!</h1>
+    <input v-if="ps" type="text" placeholder="测试1" key="11111">
+    <input v-else type="text" placeholder="测试2" key="2222">
+    <button @click="click">点击</button>
+    <ul>
+      <li v-for="(oo,name) in todos" :key="name">{{oo.id}}==={{oo.title}}=={{name}}</li>
+    </ul>
+    <input type="radio" value="oo" v-model="checkbox">
+    <input type="radio" value="iii" v-model="checkbox">
+    <p>{{checkbox}}</p>
+    <select v-model="sel">
+      <option value disabled>选择</option>
+      <option>1</option>
+      <option>2</option>
+    </select>
+    <p>no:{{sel}}</p>
+    <div class="content-block__effect" style="display:flex;justify-content: space-around">
+        <!-- <div class="portfolio-effect"> -->
+            <!-- portfolio item -->
+            <div class="portfolio-item  portfolio-item--eff1">
+                <img class="portfolio-item__image" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/694606/eff-28.jpg" alt="Portfolio Item" width="826" height="551">
+                <!-- 蒙版 -->
+                <div class="portfolio-item__info">
+                    <h3 class="portfolio-item__header">dddddd</h3>
+                    <div style="display: flex">
+                        <div class="portfolio-item__link-block">
+                            <a class="portfolio-item__link" href="#" title="">
+                                <i class="material-icons">link</i>
+                            </a>
+                        </div>
+                        <div class="portfolio-item__link-block">
+                            <a class="portfolio-item__link" href="#" title="">
+                                <i class="material-icons">cloud_download</i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="portfolio-item  portfolio-item--eff2">
+                <img class="portfolio-item__image" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/694606/eff-28.jpg" alt="Portfolio Item"
+                    width="826" height="551">
+                <!-- 蒙版 -->
+                <div class="portfolio-item__info">
+                    <h3 class="portfolio-item__header">dddddd</h3>
+                    <div style="display: flex">
+                        <div class="portfolio-item__link-block">
+                            <a class="portfolio-item__link" href="#" title="">
+                                <i class="material-icons">search</i>
+                            </a>
+                        </div>
+                        <div class="portfolio-item__link-block">
+                            <a class="portfolio-item__link" href="#" title="">
+                                <i class="material-icons">cloud_download</i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <!-- </div> -->
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      checkbox: "",
+      sel: "",
+      a: "aaaa",
+      c: "dddd",
+      ps: true,
+      object: {
+        firstName: "John",
+        lastName: "Doe",
+        age: 30
+      },
+      newTodoText: "",
+      todos: [
+        {
+          id: 1,
+          title: "Do the dishes"
+        },
+        {
+          id: 2,
+          title: "Take out the trash"
+        },
+        {
+          id: 3,
+          title: "Mow the lawn"
+        }
+      ],
+      nextTodoId: 4
+    };
+  },
+
+  created() {},
+  //进页面立即执行一次
+  mounted() {
+    const vm = this;
+    //调用方法
+    //vm.click()
+  },
+  //自定义方法
+  methods: {
+    click() {
+      this.ps = this.ps == false ? true : false;
+      // this.inte.push({ add: "33333" });
+    }
+  },
+  //监听返回值(不管有没有变化只要有这个变量就执行)
+  computed: {
+    b: {
+      //没有给b赋值的时候
+      get: function() {
+        console.log(111111);
+        return this.c + 2;
+      },
+      //给b赋值的时候
+      set: function(newValue) {
+        console.log(22222);
+        this.c = newValue;
+      }
+    }
+  },
+  //监听过程(被赋值的元素)有变化才执行
+  watch: {
+    a: function(val) {
+      console.log(val);
+    },
+    c: function(val) {
+      console.log(val);
+    },
+    b: function(val) {
+      console.log(val);
+    },
+    ps: function(val) {
+      console.log(val);
+    }
+  },
+  component: {}
+};
+</script>
+
---
+++ vue.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const vConsolePlugin = require('vconsole-webpack-plugin'); // 引入 移动端模拟开发者工具 插件 （另：https://github.com/liriliri/eruda）
+const CompressionPlugin = require('compression-webpack-plugin'); //Gzip
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin; //Webpack包文件分析器
+const baseUrl = process.env.NODE_ENV === "production" ? "/static/" : "/"; //font scss资源路径 不同环境切换控制
+
+module.exports = {
+  //基本路径
+  baseUrl: './',
+  //输出文件目录
+  outputDir: 'dist',
+  //静态资源目录
+  // assetsDir:'jjjjjj',
+  // eslint-loader 是否在保存的时候检查
+  lintOnSave: true,
+  //以多页模式构建应用程序。
+  pages: undefined,
+  //是否使用包含运行时编译器的 Vue 构建版本
+  runtimeCompiler: false,
+  //是否为 Babel 或 TypeScript 使用 thread-loader。该选项在系统的 CPU 有多于一个内核时自动启用，仅作用于生产构建，在适当的时候开启几个子进程去并发的执行压缩
+  parallel: require('os').cpus().length > 1,
+  //生产环境是否生成 sourceMap 文件，一般情况不建议打开
+  productionSourceMap: false,
+  // webpack配置
+  configureWebpack: config => {
+    //开发环境
+    let pluginsDev = [
+      //移动端模拟开发者工具(https://github.com/diamont1001/vconsole-webpack-plugin  https://github.com/Tencent/vConsole)
+      new vConsolePlugin({
+        filter: [], // 需要过滤的入口文件
+        enable: true // 发布代码前记得改回 false
+      }),
+    ];
+    if(process.env.NODE_ENV === 'production') { // 为生产环境修改配置...process.env.NODE_ENV !== 'development'
+      // config.plugins = [...config.plugins, ...pluginsPro];
+    } else {
+      // 为开发环境修改配置...
+      config.plugins = [...config.plugins, ...pluginsDev];
+    }
+  },
+  css: {
+    // 启用 CSS modules
+    modules: false,
+    // 是否使用css分离插件
+    extract: false,
+    // 开启 CSS source maps，一般不建议开启
+    sourceMap: false,
+  },
+  // webpack-dev-server 相关配置 https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    // 本地域名: 'localhost','127.0.0.0',真机测试域名:'0.0.0.0'
+    host: "0.0.0.0",
+    port: 8001, // 端口号
+    https: false, // https:{type:Boolean}
+    open: true, //配置自动启动浏览器  http://172.16.1.12:7071/rest/mcdPhoneBar/
+    hotOnly: false, // 热更新
+    // proxy: 'http://localhost:8000'   // 配置跨域处理,只有一个代理
+    proxy: { //配置自动启动浏览器
+      //配置跨域 
+      "/rest/*": {
+        target: "http://172.16.1.12:7071",
+        changeOrigin: true,
+        // ws: true,//websocket支持
+        secure: false
+      },
+      "/pbsevice/*": {
+        target: "http://172.16.1.12:2018",
+        changeOrigin: true,
+        //ws: true,//websocket支持
+        secure: false
+      },
+    }
+  },
+
+  // 第三方插件配置 https://www.npmjs.com/package/vue-cli-plugin-style-resources-loader
+  pluginOptions: {
+
+  }
+};
